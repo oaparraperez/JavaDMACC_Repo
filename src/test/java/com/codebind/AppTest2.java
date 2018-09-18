3cfe0084207b0fa0b5647cc559df05fefb566586
Oscar A Parra Perez <oaparraperez@dmacc.edu>
2018-09-18 12:12:08 -0500

    committing maven

---
+++ src/test/java/com/codebind/AppTest2.java
@@ -0,0 +1,14 @@
+package com.codebind;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest2 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
